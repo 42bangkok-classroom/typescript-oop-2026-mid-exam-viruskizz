0b2f951cc684074de80fdbe5e85c450651abdc44
Thitiwut Somsa <thitiwut.somsa@sscinc.com>
2026-01-21 23:55:05 +0700

    p09

--- src/practicals/p09/find-data.ts
+++ src/practicals/p09/find-data.ts
@@ -1,3 +1,10 @@
-function findData(fileName: string, key: string, value: string | number): void {
-  
+import fs from 'fs'
+export function findData(fileName: string, key: string, value: string | number): void {
+  try {
+    // const data = fs.readFileSync(fileName)
+  } catch (e) {
+    console.log(e);
+  }
 }
+
+// findData('./datasets/products.json');
--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,3 +1,22 @@
-function readJson(fileName: string): unknown[] {
-  
+import fs from 'fs';
+
+export function readJson(fileName: string): unknown[] {
+  try {
+    const filedata = fs.readFileSync(fileName, 'utf-8');
+    const data = JSON.parse(filedata);
+    return data;
+  } catch (e: unknown) {
+    const errName = (e as Error).name;
+    if (errName === 'Error') {
+      throw new Error('Invalid file')
+    }
+    if (errName === 'SyntaxError') {
+      throw new Error('Invalid JSON format')
+    }
+    return []
+  }
 }
+
+console.log(readJson('./src/practicals/p09/datasets/products.json'));
+console.log(readJson('./datasets/x.json'));
+console.log(readJson('./src/practicals/p09/datasets/invalid'));
