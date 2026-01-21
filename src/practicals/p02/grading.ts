aeae0678863dde9b400765a768eb8995badb1669
Thitiwut Somsa <thitiwut.somsa@sscinc.com>
2026-01-21 22:22:08 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,21 @@
 // Write your code below
+const input = +process.argv[1]
+console.log(input)
+
+if (Number.isNaN(input) || input < 0 || input > 100) {
+  console.log('Invalid Input');
+} else {
+  let grade = ''
+  if (input >= 80) {
+    grade = 'A';
+  } else if (input >= 70) {
+    grade = 'B';
+  } else if (input >= 60) {
+    grade = 'C';
+  } else if (input >= 50) {
+    grade = 'D';
+  } else {
+    grade = 'F';
+  }
+  console.log(`Grade is ${grade}`);
+}
