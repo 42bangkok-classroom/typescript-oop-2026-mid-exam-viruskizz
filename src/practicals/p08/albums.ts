eba3149114bd8b49ce1dca808e0d0f3891221fd5
Thitiwut Somsa <thitiwut.somsa@sscinc.com>
2026-01-21 23:22:26 +0700

    p08

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -1,6 +1,37 @@
-interface FullAlbum {}
+import axios from 'axios'
+export interface FullAlbum {
+  id: number;
+  userId: number;
+  title: string;
+  photos: {
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+  }[]
+}
 
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]>
+export async function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+  if (!userIds || userIds.length === 0) {
+    return new Promise(resolve => resolve([]))
+  }
+  try {
+    const photos = await axios.get(PHOTOS_URL).then(r => r.data);
+    const albums = await axios.get(ALBUMS_URL).then(r => r.data);
+    const full = albums.map((a: any) => {
+      return {
+        ...a,
+        photos: photos.filter((p: any) => p.albumId === a.id)
+      }
+    })
+    return full.filter((f: any) => userIds.includes(f.userId))
+  } catch (e) {
+    return new Promise(resolve => resolve([]))
+  }
+}
+
+// mapPhotoToAlbum([10]).then(res => console.log(res));
