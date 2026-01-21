4bf2076c2833c7c8185b8db86cc5fd9a4f96bd52
Thitiwut Somsa <thitiwut.somsa@sscinc.com>
2026-01-21 22:36:39 +0700

    p04

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,17 @@
 // Write your code below
+const m = +process.argv[2]
+if (Number.isNaN(m) || m <= 0) {
+  // console.log('Invalid Input');
+} else {
+  for (let i = 1; i<= m; i++) {
+    let line = '';
+    for (let j = m; j > 0; j--) {
+      if (j > i) {
+        line += ' ';
+      } else {
+        line += j;
+      }
+    }
+    console.log(line)
+  }
+}
